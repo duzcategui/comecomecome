504bdaef282c00a08ab4e42a268929c8e1530929
duzcategui <david.uzcategui@gmail.com>
2020-03-22 16:00:21 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,25 +3,126 @@ enum ActionKind {
     Idle,
     Jumping
 }
+function creaEnemigo () {
+    myEnemy = sprites.create(img`
+. . . . . c c c c c c c . . . . 
+. . . . c 6 7 7 7 7 7 6 c . . . 
+. . . c 7 c 6 6 6 6 c 7 6 c . . 
+. . c 6 7 6 f 6 6 f 6 7 7 c . . 
+. . c 7 7 7 7 7 7 7 7 7 7 c . . 
+. . f 7 8 1 f f 1 6 7 7 7 f . . 
+. . f 6 f 1 f f 1 f 7 7 7 f . . 
+. . . f f 2 2 2 2 f 7 7 6 f . . 
+. . c c f 2 2 2 2 7 7 6 f c . . 
+. c 7 7 7 7 7 7 7 7 c c 7 7 c . 
+c 7 1 1 1 7 7 7 7 f c 6 7 7 7 c 
+f 1 1 1 1 1 7 6 f c c 6 6 6 c c 
+f 1 1 1 1 1 1 6 6 c 6 6 6 c . . 
+f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+. f 6 1 1 1 1 1 6 6 6 6 c . . . 
+. . f f c c c c c c c c . . . . 
+`, SpriteKind.Enemy)
+    myEnemy.setPosition(Math.randomRange(10, scene.screenWidth()), Math.randomRange(10, scene.screenHeight()))
+    myEnemy.startEffect(effects.blizzard, 500)
+    myEnemy.say("JAJAJA!", 500)
+    myEnemy.follow(Mono, 10)
+    arrayEnemigos.push(myEnemy)
+    setupEnemy()
+}
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    if (Mono.overlapsWith(Hamburguesa)) {
-        info.changeScoreBy(1)
+    for (let value of Comida) {
+        posicion = Comida.indexOf(value)
+        info.changeScoreBy(puntos.indexOf(posicion))
         music.powerUp.play()
         Mono.say("Mmmm..", 1000)
-        Hamburguesa.setPosition(Math.randomRange(10, 160), Math.randomRange(10, 120))
-    } else if (Mono.overlapsWith(Pizza)) {
-        info.changeScoreBy(2)
-        music.magicWand.play()
-        Mono.say("Mmmm..x2", 1000)
-        Pizza.setPosition(Math.randomRange(10, 160), Math.randomRange(10, 120))
-    } else {
-    	
+        value.destroy()
     }
+    creaComida()
     if (info.score() >= 10) {
         music.stopAllSounds()
         game.over(true)
     }
-    if (info.score() == 3) {
+})
+function crearTodo () {
+    if (numeroEnemigos >= 5) {
+        numeroEnemigos = 5
+    }
+    for (let index = 0; index <= numeroEnemigos; index++) {
+        creaEnemigo()
+    }
+}
+function creaJugador () {
+    Mono = sprites.create(img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f . . . . . 
+. c d f d d f d e e f f . . . . 
+. c d f d d f d e e d d f . . . 
+c d e e d d d d e e b d c . . . 
+c d d d d c d d e e b d c . f f 
+c c c c c d d d e e f c . f e f 
+. f d d d d d e e f f . . f e f 
+. . f f f f f e e e e f . f e f 
+. . . . f e e e e e e e f f e f 
+. . . f e f f e f e e e e f f . 
+. . . f e f f e f e e e e f . . 
+. . . f d b f d b f f e f . . . 
+. . . f d d c d d b b d f . . . 
+. . . . f f f f f f f f f . . . 
+`, SpriteKind.Player)
+    controller.moveSprite(Mono, 100, 100)
+    info.setLife(3)
+    Mono.setPosition(Math.randomRange(10, scene.screenWidth()), Math.randomRange(10, scene.screenHeight()))
+    setupMonkey()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    music.powerDown.play()
+    scene.cameraShake(4, 500)
+    Mono.say("AUCH!", 1000)
+    destroyAll()
+    numeroEnemigos += 1
+    crearTodo()
+})
+function creaComida () {
+    if (Math.randomRange(0, 10) == 0) {
+        Taco = sprites.create(img`
+. . . . . . . . . . . . . . e e e e e e e . . . . . . . . . . . 
+. . . . . . . . . . . . e e 4 5 5 6 6 2 e 2 e . . . . . . . . . 
+. . . . . . . . . . e e 4 5 5 5 6 7 2 3 e 2 6 8 8 . . . . . . . 
+. . . . . . . . . e 4 6 7 7 6 6 7 7 2 3 2 e 7 7 7 6 6 8 . . . . 
+. . . . . . . . e 4 6 7 4 5 5 5 4 7 7 2 2 2 7 7 7 6 7 7 8 . . . 
+. . . . . . . 4 4 4 8 7 4 4 4 4 4 7 7 7 7 6 6 7 7 7 6 7 8 . . . 
+. . . . . . 4 5 2 2 e 7 7 7 7 7 7 6 7 7 7 7 6 6 6 7 6 6 6 8 . . 
+. . . . . 4 5 2 3 2 2 7 7 6 6 7 2 2 e 6 6 6 e e e e e 8 8 8 . . 
+. . . . 4 5 5 2 3 2 e 7 6 6 7 2 3 2 2 e 4 5 5 5 d d d d 4 8 . . 
+. . . 4 4 5 6 7 7 7 7 5 5 4 6 2 3 e 4 5 5 d d d d d d d d d 4 . 
+. . . e 6 6 7 7 4 5 5 4 4 7 7 e 4 5 5 d d d d 5 5 5 5 4 d d 4 4 
+. . e 4 6 7 7 7 4 4 4 6 7 7 e 5 5 d d 5 5 5 5 5 d 5 5 d d d d 4 
+. . e 5 6 6 8 6 7 7 6 6 6 e 5 d d 5 5 5 5 5 5 5 5 5 5 5 5 d d e 
+. e 4 5 5 4 4 e 8 7 7 6 e 5 d 5 5 5 5 5 4 5 5 5 5 5 5 5 5 5 d e 
+. e 5 5 4 e e e e 6 6 e 5 d 5 5 5 5 d 5 5 5 5 5 d d d d 5 4 d e 
+. e 5 5 e e 4 4 f e e 5 d 5 d 5 5 5 5 5 5 d 5 d 5 d d d d d d e 
+e 4 5 4 e e e e f e 4 5 d 5 5 5 5 5 5 5 5 5 5 5 d d 4 d d d e . 
+e 5 e 4 e e f f f e 5 d 5 5 5 5 5 5 5 5 d 5 5 5 5 d d d d e . . 
+e 5 e e 4 e e f f 4 5 d 5 5 5 5 5 5 5 5 5 5 5 5 d d d d e . . . 
+e 5 e e e e f f e 5 d 5 5 d 5 5 5 d 5 5 5 5 d 5 d d d e . . . . 
+e 5 f f e f e e e 5 d 5 5 5 4 5 5 5 5 5 5 5 d d d 4 e . . . . . 
+e 5 f f f f f f e 5 4 5 5 5 5 5 5 5 d 5 d 4 d d e e . . . . . . 
+e 5 4 e f e f f 4 5 d 5 5 d 5 5 5 5 5 d d d d e . . . . . . . . 
+e 5 e e e f f e 5 d d 5 5 5 5 5 4 5 d d d e e . . . . . . . . . 
+e 4 e e e f f f 5 d 5 5 5 5 d 5 5 d d d e . . . . . . . . . . . 
+e 4 e f e f f f 5 d 5 d 5 5 5 5 5 d 4 e . . . . . . . . . . . . 
+. e 4 e f f f e 5 d 5 5 5 5 5 5 d e e . . . . . . . . . . . . . 
+. e 5 4 e e e e 5 d 5 4 5 d d 4 e . . . . . . . . . . . . . . . 
+. . e 5 5 4 e e 5 d d d d d e e . . . . . . . . . . . . . . . . 
+. . . e e 5 5 4 4 d d d e e . . . . . . . . . . . . . . . . . . 
+. . . . . e e e e e e e . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+`, SpriteKind.Food)
+        Taco.setPosition(Math.randomRange(10, 160), Math.randomRange(10, 120))
+        Comida.push(Taco)
+    } else if (Math.randomRange(0, 10) == 1) {
         Pizza = sprites.create(img`
 . . . . . . b b b b . . . . . . 
 . . . . . . b 4 4 4 b . . . . . 
@@ -41,38 +142,51 @@ b 5 5 5 5 d d 4 4 4 4 . . . . .
 4 4 4 4 . . . . . . . . . . . . 
 `, SpriteKind.Food)
         Pizza.setPosition(Math.randomRange(10, 160), Math.randomRange(10, 120))
+        Comida.push(Pizza)
+    } else if (Math.randomRange(0, 10) == 2) {
+        cherry = sprites.create(img`
+. . . . . . . . . . . 6 6 6 6 6 
+. . . . . . . . . 6 6 7 7 7 7 8 
+. . . . . . 8 8 8 7 7 8 8 6 8 8 
+. . e e e e c 6 6 8 8 . 8 7 8 . 
+. e 2 5 4 2 e c 8 . . . 6 7 8 . 
+e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
+e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
+e 2 e e 2 2 2 2 e e e e c 6 8 . 
+c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
+. c 2 e e e 2 e 2 4 2 2 2 2 c . 
+. . c 2 2 2 e e 2 2 2 2 2 2 2 e 
+. . . e c c e c 2 2 2 2 2 2 2 e 
+. . . . . . . c 2 e e 2 2 e 2 c 
+. . . . . . . c e e e e e e 2 c 
+. . . . . . . . c e 2 2 2 2 c . 
+. . . . . . . . . c c c c c . . 
+`, SpriteKind.Food)
+        cherry.setPosition(Math.randomRange(10, 160), Math.randomRange(10, 120))
+        Comida.push(cherry)
+    } else {
+        Hamburguesa = sprites.create(img`
+. . . . c c c b b b b b . . . . 
+. . c c b 4 4 4 4 4 4 b b b . . 
+. c c 4 4 4 4 4 5 4 4 4 4 b c . 
+. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+. e b 4 4 4 4 4 5 4 4 4 4 b e . 
+8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+8 7 2 e e e e e e e e e e 2 7 8 
+e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+e b e 8 8 c c 8 8 c c c 8 e b e 
+e e b e c c e e e e e c e b e e 
+. e e b b 4 4 4 4 4 4 4 4 e e . 
+. . . c c c c c e e e e e . . . 
+`, SpriteKind.Food)
+        Hamburguesa.setPosition(Math.randomRange(10, scene.screenWidth()), Math.randomRange(10, scene.screenHeight()))
+        Comida.push(Hamburguesa)
     }
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    info.changeLifeBy(-1)
-    music.powerDown.play()
-    myEnemy.say("JAJAJA!", 500)
-    scene.cameraShake(4, 500)
-    Mono.startEffect(effects.fire, 100)
-    Mono.say("AUCH!", 1000)
-    myEnemy.setPosition(Math.randomRange(10, 160), Math.randomRange(10, 120))
-    myEnemy = sprites.create(img`
-. . . . . c c c c c c c . . . . 
-. . . . c 6 7 7 7 7 7 6 c . . . 
-. . . c 7 c 6 6 6 6 c 7 6 c . . 
-. . c 6 7 6 f 6 6 f 6 7 7 c . . 
-. . c 7 7 7 7 7 7 7 7 7 7 c . . 
-. . f 7 8 1 f f 1 6 7 7 7 f . . 
-. . f 6 f 1 f f 1 f 7 7 7 f . . 
-. . . f f 2 2 2 2 f 7 7 6 f . . 
-. . c c f 2 2 2 2 7 7 6 f c . . 
-. c 7 7 7 7 7 7 7 7 c c 7 7 c . 
-c 7 1 1 1 7 7 7 7 f c 6 7 7 7 c 
-f 1 1 1 1 1 7 6 f c c 6 6 6 c c 
-f 1 1 1 1 1 1 6 6 c 6 6 6 c . . 
-f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
-. f 6 1 1 1 1 1 6 6 6 6 c . . . 
-. . f f c c c c c c c c . . . . 
-`, SpriteKind.Enemy)
-    myEnemy.follow(Mono, 10)
-    myEnemy.setPosition(Math.randomRange(10, 160), Math.randomRange(10, 120))
-    setupEnemy()
-})
+}
 function setupMonkey () {
     MonkeyFrames = [sprites.builtin.forestMonkey0, sprites.builtin.forestMonkey1]
     animation.runImageAnimation(
@@ -91,81 +205,38 @@ function setupEnemy () {
     true
     )
 }
-info.onLifeZero(function () {
-    music.stopAllSounds()
-    Mono.destroy(effects.disintegrate, 500)
-    game.over(false)
-})
+function destroyAll () {
+    effects.starField.startScreenEffect(500)
+    for (let value of arrayEnemigos) {
+        value.destroy(effects.spray, 10)
+    }
+    for (let value of Comida) {
+        value.destroy(effects.spray, 10)
+    }
+}
 let EnemyFrames: Image[] = []
 let MonkeyFrames: Image[] = []
-let Pizza: Sprite = null
 let Hamburguesa: Sprite = null
-let myEnemy: Sprite = null
+let cherry: Sprite = null
+let Pizza: Sprite = null
+let Taco: Sprite = null
+let posicion = 0
 let Mono: Sprite = null
+let myEnemy: Sprite = null
+let puntos: number[] = []
+let Comida: Sprite[] = []
+let arrayEnemigos: Sprite[] = []
+let numeroEnemigos = 0
 scene.setBackgroundColor(5)
 music.baDing.play()
-Mono = sprites.create(img`
-. . . . f f f f f . . . . . . . 
-. . . f e e e e e f . . . . . . 
-. . f d d d d e e e f . . . . . 
-. c d f d d f d e e f f . . . . 
-. c d f d d f d e e d d f . . . 
-c d e e d d d d e e b d c . . . 
-c d d d d c d d e e b d c . f f 
-c c c c c d d d e e f c . f e f 
-. f d d d d d e e f f . . f e f 
-. . f f f f f e e e e f . f e f 
-. . . . f e e e e e e e f f e f 
-. . . f e f f e f e e e e f f . 
-. . . f e f f e f e e e e f . . 
-. . . f d b f d b f f e f . . . 
-. . . f d d c d d b b d f . . . 
-. . . . f f f f f f f f f . . . 
-`, SpriteKind.Player)
-controller.moveSprite(Mono, 100, 100)
-info.setLife(3)
-myEnemy = sprites.create(img`
-. . . . . c c c c c c c . . . . 
-. . . . c 6 7 7 7 7 7 6 c . . . 
-. . . c 7 c 6 6 6 6 c 7 6 c . . 
-. . c 6 7 6 f 6 6 f 6 7 7 c . . 
-. . c 7 7 7 7 7 7 7 7 7 7 c . . 
-. . f 7 8 1 f f 1 6 7 7 7 f . . 
-. . f 6 f 1 f f 1 f 7 7 7 f . . 
-. . . f f 2 2 2 2 f 7 7 6 f . . 
-. . c c f 2 2 2 2 7 7 6 f c . . 
-. c 7 7 7 7 7 7 7 7 c c 7 7 c . 
-c 7 1 1 1 7 7 7 7 f c 6 7 7 7 c 
-f 1 1 1 1 1 7 6 f c c 6 6 6 c c 
-f 1 1 1 1 1 1 6 6 c 6 6 6 c . . 
-f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
-. f 6 1 1 1 1 1 6 6 6 6 c . . . 
-. . f f c c c c c c c c . . . . 
-`, SpriteKind.Enemy)
-myEnemy.follow(Mono, 10)
-myEnemy.setPosition(Math.randomRange(10, 160), Math.randomRange(10, 120))
 game.splash("10 to win!", "")
-Hamburguesa = sprites.create(img`
-. . . . c c c b b b b b . . . . 
-. . c c b 4 4 4 4 4 4 b b b . . 
-. c c 4 4 4 4 4 5 4 4 4 4 b c . 
-. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
-e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
-e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
-e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
-. e b 4 4 4 4 4 5 4 4 4 4 b e . 
-8 7 e e b 4 4 4 4 4 4 b e e 6 8 
-8 7 2 e e e e e e e e e e 2 7 8 
-e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
-e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
-e b e 8 8 c c 8 8 c c c 8 e b e 
-e e b e c c e e e e e c e b e e 
-. e e b b 4 4 4 4 4 4 4 4 e e . 
-. . . c c c c c e e e e e . . . 
-`, SpriteKind.Food)
-Hamburguesa.setPosition(Math.randomRange(10, 160), Math.randomRange(10, 120))
-setupMonkey()
-setupEnemy()
+numeroEnemigos = 0
+arrayEnemigos = sprites.allOfKind(SpriteKind.Enemy)
+Comida = sprites.allOfKind(SpriteKind.Food)
+puntos = []
+creaJugador()
+crearTodo()
+creaComida()
 forever(function () {
     music.playMelody("B A G A G F A C5 ", 322)
 })
